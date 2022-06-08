b0c9fa37974b7f168921eb5672db8c088c9247ed
thebeyondr <jeopatto@gmail.com>
2022-06-07 23:37:20 -0500

    feat: :sparkles: Add the column and zoom in mate

--- code.ts
+++ code.ts
@@ -60,12 +60,36 @@ const __getParameterSuggestions = (key: string): string[] => {
     }[key];
 };
 
-const _parseParameters = (
+const __loadFonts = async (textBox: TextNode): Promise<void> => {
+    try {
+        await Promise.all(
+            textBox
+                .getRangeAllFontNames(0, textBox.characters.length)
+                .map(figma.loadFontAsync)
+        );
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const __formatParams = (
     textBox: TextNode,
     numColumns: string,
     spacing: string,
     colWidth: string
-): void => {
+): {
+    numColumns: number;
+    spacing: number;
+    textBox: {
+        text: string;
+        posX: number;
+        posY: number;
+        width: number;
+        height: number;
+        id: string;
+    };
+    colWidth: string | number;
+} => {
     const parameters = {
         numColumns: parseInt(numColumns),
         spacing: parseInt(spacing),
@@ -75,11 +99,31 @@ const _parseParameters = (
             posX: textBox.x,
             posY: textBox.y,
             width: textBox.width,
+            height: textBox.height,
+            id: textBox.id,
+            // styledSegments: textBox.getStyledTextSegments(['fontName']),
         },
         colWidth: colWidth ? parseInt(colWidth) : 'auto',
     };
 
-    console.log(parameters);
+    return parameters;
+};
+
+const generateTextNodes = async (
+    formattedParams: any,
+    columnFrame: FrameNode
+) => {
+    for (let i = 0; i < formattedParams.numColumns; i++) {
+        let textNode = figma.createText();
+        if (textNode.hasMissingFont) {
+            return figma.closePlugin(
+                'Please ensure the text box has a font and try again 😢'
+            );
+        }
+        await __loadFonts(textNode);
+        textNode.characters = formattedParams.textBox.text;
+        columnFrame.appendChild(textNode);
+    }
 };
 
 const runPlugin = (textBox: TextNode): void => {
@@ -105,13 +149,38 @@ const runPlugin = (textBox: TextNode): void => {
             return;
         }
 
-        _parseParameters(textBox, numColumns, spacing, colWidth);
+        const formattedParams = __formatParams(
+            textBox,
+            numColumns,
+            spacing,
+            colWidth
+        );
+        console.log(textBox);
+
+        console.log(formattedParams);
+
         // TODO: Load fonts
-        // TODO: Create text nodes
+
         // TODO: Create auto-layout with spacing/column width
+        let columnFrame = figma.createFrame();
+        columnFrame.name = 'Columns';
+        // Append the Meta card and place next to the node
+        let node = figma.getNodeById(formattedParams.textBox.id) as SceneNode;
+        figma.currentPage.appendChild(columnFrame);
+        columnFrame.x = node.x;
+        columnFrame.y = node.y + node.height + formattedParams.spacing;
+        columnFrame.layoutMode = 'HORIZONTAL';
+        columnFrame.itemSpacing = formattedParams.spacing;
+        console.log(columnFrame);
+
+        // TODO: Create text nodes
+        generateTextNodes(formattedParams, columnFrame);
+
         // TODO: Place auto-layout frame at original coordinates
         // TODO: Highlight auto-layout frame
+        figma.currentPage.selection = [columnFrame];
         // TODO: Zoom to auto-layout frame
+        figma.viewport.scrollAndZoomIntoView(figma.currentPage.selection);
     });
 };
 
@@ -128,16 +197,13 @@ const __checkSelection = (): void => {
     }
 
     if (numErrors > 0) {
-        figma.closePlugin('Select ONE box with TEXT in it and try again 😢');
+        figma.closePlugin('Select a text box and try again 😉');
         return;
     }
 
     const textNode = currentSelection[0] as TextNode;
-    textNode.getRangeFontName(0, 5);
+
     runPlugin(textNode);
 };
 
 __checkSelection();
-
-// figma.currentPage.selection = nodes;
-// figma.viewport.scrollAndZoomIntoView(nodes);
